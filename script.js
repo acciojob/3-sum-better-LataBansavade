a417eff7415751b1bd986c82e36f0edf119a528d
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-09-29 13:11:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-function threeSum(arr, target) {
-// write your code here
+function threeSum(nums, target) {
+// write your code herenums.sort((a, b) => a - b);
+
+    let closestSum = Infinity; // Initialize the closest sum to a large value
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            // If the current sum is closer to the target, update closestSum
+            if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                closestSum = sum;
+            }
+
+            // Adjust the pointers based on the comparison with the target
+            if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return closestSum;
   
 }
 
-module.exports = threeSum;
+
